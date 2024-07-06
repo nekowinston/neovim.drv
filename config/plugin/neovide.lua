a13094a85ad6259c63d474e4322a450ad24a01e7
winston <hey@winston.sh>
2024-07-06 02:33:38 +0200

    feat: update neovide option names

--- config/plugin/neovide.lua
+++ config/plugin/neovide.lua
@@ -2,18 +2,18 @@ if not vim.g.neovide then
 	return
 end
 
-local system = vim.loop.os_uname().sysname
+local system = vim.uv.os_uname().sysname
 
 vim.opt.guifont = "Berkeley Mono,Symbols Nerd Font:h14:#e-subpixelantialias"
 vim.g.neovide_cursor_vfx_mode = "ripple"
 vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_input_macos_alt_is_meta = system == "Darwin"
+vim.g.neovide_input_macos_option_key_is_meta = system == "Darwin" and "only_left" or "none"
 vim.g.neovide_refresh_rate = 60
 vim.g.neovide_refresh_rate_idle = 5
 vim.g.neovide_scroll_animation_length = 0.3
 
 -- options only currently available on macOS
-if vim.loop.os_uname().sysname == "Darwin" then
+if system == "Darwin" then
 	vim.g.neovide_transparency = 0.8
 	vim.g.neovide_window_blurred = true
 end
