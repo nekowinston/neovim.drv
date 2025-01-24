2a5cede8b118a6ce7272d5310ce6c8fba4e4d648
winston <hey@winston.sh>
2025-01-25 00:56:14 +0100

    feat(lualine): cleanup & change it a bit

--- plugins/lualine.lua
+++ plugins/lualine.lua
@@ -13,40 +13,35 @@ end
 
 return function()
 	require("lualine").setup({
-		inactive_winbar = {
-			lualine_c = {
-				{ "navic", draw_empty = true },
-			},
-		},
 		options = {
-			component_separators = {
-				left = "",
-				right = "",
-			},
-			disabled_filetypes = {
-				statusline = { "alpha" },
-				winbar = { "alpha" },
-			},
-			icons_enabled = true,
-			section_separators = {
-				left = "",
-				right = "",
-			},
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
 			theme = "milspec",
 		},
 		sections = {
-			lualine_a = { "mode" },
+			lualine_a = {
+				{
+					"mode",
+					fmt = function(str)
+						return str:sub(1, 1)
+					end,
+				},
+			},
 			lualine_b = {
 				{ "branch", fmt = shorten },
 				"diff",
-				"diagnostics",
 			},
-			lualine_c = { "searchcount" },
+			lualine_c = { "diagnostics" },
 			lualine_x = { "filetype" },
 			lualine_y = { "progress" },
 			lualine_z = { "location" },
 		},
 		winbar = {
+			lualine_c = {
+				{ "navic", draw_empty = true },
+			},
+		},
+		inactive_winbar = {
 			lualine_c = {
 				{ "navic", draw_empty = true },
 			},
