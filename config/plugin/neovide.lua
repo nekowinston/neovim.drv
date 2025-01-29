114a6ed25a28231dc3efd996cebaf153b675a93c
winston <hey@winston.sh>
2025-01-29 19:28:10 +0100

    refactor(neovide): move system specific stuff around

--- config/plugin/neovide.lua
+++ config/plugin/neovide.lua
@@ -7,15 +7,8 @@ local system = vim.uv.os_uname().sysname
 
 vim.g.neovide_cursor_vfx_mode = "ripple"
 vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_input_macos_option_key_is_meta = system == "Darwin" and "only_left" or "none"
 
--- options only currently available on macOS
-if system == "Darwin" then
-	vim.g.neovide_transparency = 0.95
-	vim.g.neovide_window_blurred = true
-end
-
-vim.keymap.set("n", "<M-CR>", function()
+map("n", "<M-CR>", function()
 	vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
 end)
 
@@ -26,17 +19,23 @@ local change_scale_factor = function(delta)
 	vim.cmd.redraw()
 end
 
-vim.keymap.set("n", "<D-=>", function()
+map("n", "<D-=>", function()
 	change_scale_factor(1.25)
 end)
-vim.keymap.set("n", "<D-->", function()
+map("n", "<D-->", function()
 	change_scale_factor(1 / 1.25)
 end)
 
 if system == "Darwin" then
+	-- options only available on macOS
+	vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+	vim.g.neovide_transparency = 0.95
+	vim.g.neovide_window_blurred = true
+	-- save with ⌘+S
 	map("n", "<D-s>", ":w<CR>")
+	-- clipboard with ⌘+C and ⌘+V
 	map("v", "<D-c>", '"+y')
 	map({ "n", "v" }, "<D-v>", '"+p')
 	map("c", "<D-v>", "<C-R>+")
-	map("i", "<D-v>", '<esc>l"+pa')
+	map("i", "<D-v>", '<Esc>l"+pa')
 end
